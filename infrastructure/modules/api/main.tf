cb3edddb4b0589531184bbed5b2ae59268c3a387
Andraz Brodnik <brodul@brodul.org>
2023-07-10 14:55:22 +0200

    Rewrite TF to module

--- infrastructure/modules/api/main.tf
+++ infrastructure/modules/api/main.tf
@@ -1,10 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.52.0"
+    }
+  }
+}
+
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_repository
 resource "aws_ecr_repository" "spork" {
   name                 = "spork-app"
   image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
 }

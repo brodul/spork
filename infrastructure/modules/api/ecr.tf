ee15e626e3fb8599f42d0c83afb4ef0ef7d5d932
Andraz Brodnik <brodul@brodul.org>
2023-07-14 16:20:03 +0200

    Fixes

--- infrastructure/modules/api/ecr.tf
+++
@@ -1,9 +0,0 @@
-###
-# ECR
-###
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_repository
-resource "aws_ecr_repository" "spork_2" {
-  name                 = "spork-app-ecs"
-  image_tag_mutability = "MUTABLE"
-}
--- infrastructure/modules/api/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "ecr_url" {
-  value = aws_ecr_repository.spork_2.repository_url
-}
-
-output "service_url" {
-  value = aws_apprunner_service.spork.service_url
-
-}

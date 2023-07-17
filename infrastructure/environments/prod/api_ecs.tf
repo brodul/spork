aa67adbcf707de62b464f3b9952580555df09f67
Andraz Brodnik <brodul@brodul.org>
2023-07-17 11:05:35 +0200

    Remove module for now

--- infrastructure/environments/prod/api_ecs.tf
+++ infrastructure/environments/prod/api_ecs.tf
@@ -1,7 +1,7 @@
-module "api_ecs" {
-  source = "../../modules/api_ecs"
-}
+# module "api_ecs" {
+#   source = "../../modules/api_ecs"
+# }
 
-output "ecr_ecs_url" {
-  value = module.api_ecs.ecr_url
-}
+# output "ecr_ecs_url" {
+#   value = module.api_ecs.ecr_url
+# }
